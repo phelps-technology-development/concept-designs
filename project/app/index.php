46123977f8d77f418e1a38bce1d920ebd4393ec0
William Phelps <william@phelpstechdev.com>
2019-10-10 06:32:21 -0600

    Update index.php

--- project/app/index.php
+++ project/app/index.php
@@ -15,6 +15,7 @@ if (isset($_SESSION['user_id'])) {
 <head>
   <title>Opus Dashboard</title>
   <link rel="stylesheet" href="https://www.phelpstechdev.com/v2/assets/plugin/styles/version0.1.0/scss/main.css">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 </head>
 <body class="font-trebuchet bg-dark text-light">
   <div class="heroImage heroImage-full height-100" style="background-image: url('../assets/images/coast.jpg');">
@@ -23,7 +24,7 @@ if (isset($_SESSION['user_id'])) {
         <div class="card bg-light box-shadow-dark-60">
           <div class="container-30">
             <h1>Welcome, <?php echo $user['fname'] . " " . $user['lname']; ?>!</h1>
-            <a href="create/team.php"><button class="btn bg-green text-light container-15 font-size-15">Create a Team</button></a> <a href="create/project.php"><button class="btn bg-green text-light container-15 font-size-15">Create a Project</button></a> <a href="create/task.php"><button class="btn bg-green text-light container-15 font-size-15">Create a Task</button></a>
+            <a href="create/team.php"><button class="btn bg-green text-light container-15 font-size-15"><i class="material-icons">person_add</i> Create a Team</button></a> <a href="create/project.php"><button class="btn bg-green text-light container-15 font-size-15">Create a Project</button></a> <a href="create/task.php"><button class="btn bg-green text-light container-15 font-size-15">Create a Task</button></a>
           </div>
         </div>
         <div class="card bg-light box-shadow-dark-60">
